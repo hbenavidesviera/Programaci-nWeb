5d5ac54d10acdadbe60e91e07fdb7c10f162e475
hbenavidesviera <135678013+hbenavidesviera@users.noreply.github.com>
2023-07-08 22:46:36 -0400

    Update Productos.php

---
+++ Productos.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="nav">
+  <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <a class="nav-link" href="Index.php">Inicio</a>
+  <a class="nav-link" href="Productos.php">Productos</a>
+  <a class="nav-link" href="Contacto.php">Contacto</a>
+  <a class="nav-link" href="acceso.php">Login</a>
+
+<h1> Nuestros productos</h1>
+<?php
+$serverName = "LAPTOP-2RMDJCNI\SQLEXPRESS";
+$user = "LAPTOP-2RMDJCNI\hecto";
+$password = "";
+$database = "Tienda";
+$conn = mysqli_connect($serverName, $user, $password, $database )
+?> 
+
+<br> <br>
+<table>
+    <tr>
+        <td>Nombre</td>
+        <td>precio</td>
+    </tr>
+
+    <?php 
+    $sql = "SELECT * from Producto";
+    $result = mysqli_query($conn, $sql);
+    
+    while($mostrar= mysqli_fetch_array($result)){
+    ?>    
+
+   
+    
+    <tr>
+        <td><?php echo $mostrar['Nombre'] ?></td>
+        <td><?php echo $mostrar['precio'] ?> </td>
+    </tr>
+<?php    
+}
+?>
+</table>    
+    
+</body>
+</html>
