a599ffb8f3fa9290879b40859cd172b20a0555fe
hbenavidesviera <135678013+hbenavidesviera@users.noreply.github.com>
2023-07-09 18:54:47 -0400

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,48 +1,18 @@
 <?php
-//Iniciamos la session
-session_start();
+$serverName = "localhost";
+$user = "root";
+$password = "root";
+$database = "tiendamm";
+$conn = mysqli_connect($serverName, $user, $password, $database );
 
-// Validar el inicio de sesión
-if (!isset($_SESSION['rut'])) {
-    // Redirigir al formulario de inicio de sesión si no hay sesión activa
-    header('Location: acceso.php');
-    exit();
-}
+$id= $_GET['id'];
+
+$sql= "DELETE FROM producto WHERE idProducto='$id'";
+
+$query = mysqli_query($conn, $sql);
+if($query){
+    Header("Location: panelAdmin.php");
+};
+
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<table class="table table-striped">
-    <tr>
-        <td>Nombre</td>
-        <td>precio</td>
-        <td></td>
-        <td></td>
-    </tr>
-
-    <?php 
-    $sql = "SELECT * from producto";
-    $result = mysqli_query($conn, $sql);
-    
-    while($mostrar= mysqli_fetch_array($result)){
-    ?>    
-
-   
-    
-    <tr>
-        <td><?php echo $mostrar['Nombre'] ?></td>
-        <td><?php echo $mostrar['Precio'] ?> </td>
-        <td></td>
-        <td></td>
-    </tr>
-<?php    
-}
-?>
-</table> 
-</body>
-</html>
