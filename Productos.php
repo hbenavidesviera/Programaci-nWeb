75f61354423f6f23df1d2753c5e5b26665addb4e
hbenavidesviera <135678013+hbenavidesviera@users.noreply.github.com>
2023-07-09 00:38:25 -0400

    Update Productos.php

--- Productos.php
+++ Productos.php
@@ -4,16 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
-<body>
-<nav class="nav">
+
+<nav class="nav" class="navbar navbar-expand-lg bg-body-tertiary">
   <a class="nav-link active" aria-current="page" href="#">Active</a>
   <a class="nav-link" href="Index.php">Inicio</a>
   <a class="nav-link" href="Productos.php">Productos</a>
   <a class="nav-link" href="Contacto.php">Contacto</a>
   <a class="nav-link" href="acceso.php">Login</a>
-
+</nav>
+<br><br><br>
 <h1> Nuestros productos</h1>
+<body>
 <?php
 $serverName = "localhost";
 $user = "root";
@@ -23,7 +27,7 @@ $conn = mysqli_connect($serverName, $user, $password, $database )
 ?> 
 
 <br> <br>
-<table>
+<table class="table table-striped">
     <tr>
         <td>Nombre</td>
         <td>precio</td>
